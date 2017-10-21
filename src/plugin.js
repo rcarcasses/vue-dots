669e55e0f73171e615998e0dd5241342670728f9
Robert Carcassés Quevedo <carcasses@gmail.com>
2017-10-21 18:09:46 +0100

    Store is now registered in the vue instance in the $vueDots attribute

--- src/plugin.js
+++ src/plugin.js
@@ -3,5 +3,6 @@ import VueDots from './App.vue'
 module.exports = {
   install: function (Vue, options) {
     Vue.component('vue-dots', VueDots)
+    Vue.prototype.$vueDots = VueDots.data().store
   }
 }
